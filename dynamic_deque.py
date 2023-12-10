8016dd52587ebfdfefc9126339a2c70a37d90b1d
NikolaiKrustev03 <n.krastev2003@gmail.com>
2023-12-10 16:12:00 +0200

    My University Term Paper
    My Term Paper in Algorithm Synthesis and Analysis

---
+++ dynamic_deque.py
@@ -0,0 +1,61 @@
+class DynamicDeque:
+
+    def __init__(self):
+        self.deque = []
+
+    def is_empty(self):
+        return not bool(self.deque)
+
+    def appendleft(self, x):
+        self.deque.insert(0, x)
+
+    def append(self, x):
+        self.deque.append(x)
+
+    def clear(self):
+        if self.deque:
+            self.deque = []
+            return "Deque cleared"
+        else:
+            return "Deque already cleared"
+    def copy(self):
+        new_deque = DynamicDeque()
+        new_deque.deque = self.deque.copy()
+        return new_deque
+
+    def count(self, x):
+        return f"Item {x} is found {self.deque.count(x)} times"
+
+    def insert(self, i, x):
+        self.deque.insert(i, x)
+
+    def extend(self, iterable):
+        for element in iterable:
+            self.append(element)
+
+    def extendleft(self, iterable):
+        for element in iterable:
+            self.appendleft(element)
+
+
+    def pop(self):
+        if not self.is_empty():
+            return self.deque.pop()
+        else:
+            raise ValueError("Cannot pop from empty deque")
+
+    def popleft(self):
+        if not self.is_empty():
+            return self.deque.pop(0)
+        else:
+            raise ValueError("Cannot pop from empty deque")
+
+    def __str__(self):
+        return " ".join(str(x) for x in self.deque)
+
+# Example usage:
+dynamic_deque = DynamicDeque()
+
+dynamic_deque.appendleft(1)
+print(dynamic_deque.clear())
+
